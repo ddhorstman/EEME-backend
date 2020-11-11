6f15475be4011486399aba5e2594f822e51fcb39
David Horstman <davidzilla27@gmail.com>
2020-11-10 17:00:14 -0800

    Initialize with Spring Boot.

---
+++ src/main/java/com/davidhorstman/eeme/EemeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.davidhorstman.eeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EemeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EemeBackendApplication.class, args);
+    }
+
+}
