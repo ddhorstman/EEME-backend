e9a201d1f4dfbd3f19228c0987c8613467516be1
David Horstman <davidzilla27@gmail.com>
2020-11-10 21:24:09 -0800

    Add config for datasources.

---
+++ src/main/java/com/davidhorstman/eeme/config/DataSourceConfig.java
@@ -0,0 +1,41 @@
+package com.davidhorstman.eeme.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${local.run.db:h2}")
+    private String dbType;
+
+    @Value("${spring.datasource.url:}")
+    private String dbURL;
+
+    @Bean
+    public DataSource dataSource(){
+        if(dbType.equalsIgnoreCase("postgresql")){
+            HikariConfig config = new HikariConfig();
+            config.setDriverClassName("org.postgresql.Driver");
+            config.setJdbcUrl(dbURL);
+            return new HikariDataSource(config);
+        } else {
+            String urlString = "jdbc:mem:testdb";
+            String driverClass = "org.h2.Driver";
+            String dbUser = "sa";
+            String password = "leaveitatdefault";
+            return DataSourceBuilder.create()
+                    .username(dbUser)
+                    .password(password)
+                    .url(urlString)
+                    .driverClassName(driverClass)
+                    .build();
+        }
+    }
+}
